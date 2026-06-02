3190c2997fa8c912aaa7779caec643dba84607fc
Abdullah Alsasa <abdullahalsasa@Abdullahs-MacBook-Pro.local>
2026-06-02 16:42:21 +0300

    Fix HeroSliderResource translation error by adding Translatable trait

--- app/Filament/Resources/HeroSliders/HeroSliderResource.php
+++ app/Filament/Resources/HeroSliders/HeroSliderResource.php
@@ -13,9 +13,11 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use LaraZeus\SpatieTranslatable\Resources\Concerns\Translatable;
 
 class HeroSliderResource extends Resource
 {
+    use Translatable;
     protected static ?string $model = HeroSlider::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
