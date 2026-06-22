aa21ed93fc892ea37a42597ef1a62feb4f832a74
Abdullah Alsasa <abdullahalsasa@Abdullahs-MacBook-Pro.local>
2026-06-22 18:45:26 +0300

    Add Create/New buttons to My Products and My Stock Deals list pages

--- app/Filament/Factory/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Factory/Resources/ProductResource/Pages/ListProducts.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Factory\Resources\ProductResource\Pages;
 
 use App\Filament\Factory\Resources\ProductResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProducts extends ListRecords
 {
     protected static string $resource = ProductResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
--- app/Filament/Factory/Resources/StockOfferResource/Pages/ListStockOffers.php
+++ app/Filament/Factory/Resources/StockOfferResource/Pages/ListStockOffers.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Factory\Resources\StockOfferResource\Pages;
 
 use App\Filament\Factory\Resources\StockOfferResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListStockOffers extends ListRecords
 {
     protected static string $resource = StockOfferResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
